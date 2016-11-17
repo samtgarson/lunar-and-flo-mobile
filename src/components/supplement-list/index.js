78658c5b4c0ae5ee59b8ef0a2e490433610dc7c3
Sam Garson <samtgarson@gmail.com>
2016-11-17 22:03:46 +0000

    WIP search animation

--- src/components/supplement-list/index.js
+++ src/components/supplement-list/index.js
@@ -1,43 +1,84 @@
 import React, { Component } from 'react';
-import { ListView, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { ListView, Text, View, TouchableOpacity, TextInput, ScrollView } from 'react-native';
 import globalStyles from '../styles';
 import Pack from '../pack';
+import * as Animatable from 'react-native-animatable';
 
 const styles= {
   row: {
     paddingVertical: 16
   },
+  searchContainer: {
+    position: 'absolute',
+    top: -80,
+    left: 0,
+    right: 0,
+    padding: 10,
+    paddingTop: 30,
+    zIndex: 1
+  },
   search: {
     height: 40,
     backgroundColor: 'white'
+  },
+  list: {
+    paddingTop: 40
   }
 }
 
-const renderPack = (props) => {
-  const { pack, selectSupplement } = props
-  return <Pack selectSupplement={selectSupplement} pack={pack} />
-}
+export default class SupplementList extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      previousOffset: 0,
+      scrollScore: 0,
+      showSearch: true
+    }
+  }
+  renderPack () {
+    const { pack, selectSupplement } = this.props
+    return <Pack selectSupplement={selectSupplement} pack={pack}/>
+  }
 
-const renderRow = (props) => (row) => {
-  if (row === true) return renderPack(props)
-  return (
-    <TouchableOpacity style={styles.row} onPress={() => props.selectSupplement(row.id)}>
-      <Text>{ row.name }</Text>
-    </TouchableOpacity>
-  )
-}
+  renderRow (row) {
+    if (row.id == '__pack') return this.renderPack()
+    return (
+      <TouchableOpacity style={styles.row} onPress={() => this.props.selectSupplement(row.id)}>
+        <Text>{ row.name }</Text>
+      </TouchableOpacity>
+    )
+  }
 
-export default SupplementList = (props) => {
-  const { supplements, updateSearchTerm, searchTerm } = props
-  const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1.id ? r1.id !== r2.id : r1 !== r2});
-  const source = supplements.unshift(true)
+  handleScroll (event) {
+    const [fwd, bck] = [1, 0]
+    const currentOffset = event.nativeEvent.contentOffset.y;
+    const direction = currentOffset > this.state.previousOffset ? fwd : bck;
+    const score = direction == fwd ? 1 : this.state.scrollScore - 1
+    this.setState({ 
+      scrollScore: score,
+      previousOffset: currentOffset, 
+      showSearch: score <= -5 || currentOffset < 20
+    })
+  }
 
-  return (
-    <View style={globalStyles.screen}>
-      <TextInput onChangeText={ updateSearchTerm } style={styles.search} value={searchTerm} autoCorrect={false} />
-      <ListView dataSource={ds.cloneWithRows(props.supplements)} renderRow={renderRow(props)} />
-    </View>
-  )
+  render () {
+    const { supplements, updateSearchTerm, searchTerm } = this.props
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1.id !== r2.id});
+
+    return (
+      <View style={globalStyles.screen}>
+        <Animatable.View transition="top" style={{...styles.searchContainer, top: this.state.showSearch ? 0 : -80}}>
+          <TextInput onChangeText={updateSearchTerm} style={styles.search} value={searchTerm} autoCorrect={false} />
+        </Animatable.View>
+        <ScrollView transition="paddingTop" scrollEventThrottle={100} style={{
+          ...styles.list
+        }} onScroll={this.handleScroll.bind(this)}>
+          {this.renderPack.bind(this)()}
+          <ListView dataSource={ds.cloneWithRows(this.props.supplements)} renderRow={this.renderRow.bind(this)} />
+        </ScrollView>
+      </View>
+    )
+  }
 }
 
 SupplementList.propTypes = {
